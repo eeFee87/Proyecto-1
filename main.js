7f1848fa8202d4702a11f0ad9199b9ef207a0f74
unknown <eefee1987@gmail.com>
2022-11-12 11:56:02 +0100

    tarjeta animada

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 'use strict';
+const cards = document.querySelectorAll('.card');
+
+const reveal = (e) => {
+  const currentCard = e.currentTarget;
+  currentCard.classList.add('flipped');
+
+  setTimeout(() => {
+    currentCard.classList.remove('flipped');
+  }, 1000);
+};
+
+for (const card of cards) {
+  card.addEventListener('click', reveal);
+}
